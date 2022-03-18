449b878fc281d1d72cb38ac9dce36224ed854dd5
JoeBlock99 <blockmann3@gmail.com>
2022-03-18 00:01:12 -0600

    ya

--- utilities.py
+++ utilities.py
@@ -1,7 +1,3 @@
-def Diff(li1, li2):
-    li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
-    return li_dif
-
 def GetTransitions(transitions):
     trans = ''
     for key, value in transitions.items():
